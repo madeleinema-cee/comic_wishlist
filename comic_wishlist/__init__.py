46ab6ae13910cec9bd408ec81b1a4467b8c94dcb
madeleine <yuema0415@gmail.com>
2020-07-02 16:26:17 -0400

    added template, css

--- comic_wishlist/__init__.py
+++ comic_wishlist/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
-
 from comic_wishlist import routes
+
+
--- run.py
+++ run.py
@@ -3,3 +3,4 @@ from comic_wishlist import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+
