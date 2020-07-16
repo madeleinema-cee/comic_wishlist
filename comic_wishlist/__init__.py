0d23cad64b9dcda0f7642c56c4e8cb19d1ee4213
mayue3081249 <yuema0415@gmail.com>
2020-07-16 16:27:56 -0400

    added fake data and created format

--- comic_wishlist/__init__.py
+++ comic_wishlist/__init__.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'ea04b6d14a74c959fd88b404ed144bd5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from comic_wishlist import db
-from comic_wishlist.models import Colors, Comics
+from comic_wishlist.models import Colors, Comics, Issues, Presses
 
 colors = {
     'steel': ['#14213d', '#fca311', '#282e3c', '#fca311', '#284b63'],
@@ -15,18 +15,48 @@ def add_color():
         db.session.add(color)
 
 
-comics = {
-    'ironman': ['Marvel']
-}
+presses = ['Marvel', 'DC', 'A03']
 
+def add_press():
+    for x in presses:
+        press = Presses(name=x)
+        db.session.add(press)
+        db.session.commit()
+
+
+comics = {
+    'ironman': [1],
+    'x-men': [1],
+    'superman': [2]
+}
 
 def add_comic():
     for k, v in comics.items():
         comic = Comics(title=k, press=v[0])
 
         db.session.add(comic)
-        db.session.commit()
 
 
+issues = {
+    'ironman vs cap #109': ['109', 2009, 1],
+    'ironman vs cap #809': ['809', 2020, 1],
+    'x-men special': ['400', 1996, 2],
+    'superman loves batman': ['10000', 2005, 3]
+}
+
+
+def add_issue():
+    for k, v in issues.items():
+        issue = Issues(sub_title=k, number=v[0], year=v[1], comic_id=v[2])
+        db.session.add(issue)
+
+
+
+
+
+db.session.commit()
+add_color()
 add_comic()
+add_issue()
+add_press()
 
