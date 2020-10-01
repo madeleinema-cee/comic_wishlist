de65c2b76051d6db748066f0dad30b3d01c8b459
madeleine <yuema0415@gmail.com>
2020-10-01 17:06:41 -0400

    added config

--- comic_wishlist/__init__.py
+++ comic_wishlist/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '#'
+app.config['SECRET_KEY'] = config.secret['secret_key']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 login_manager = LoginManager(app)
